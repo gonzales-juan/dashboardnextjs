69bade74c8c7ad7b3da14403f1e9c93eb8c83321
Juan-Carlos <juan-carlos.gonzales@technisem.com>
2024-02-29 09:59:11 +0100

    update getuser

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -229,7 +229,7 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  noStore()
+
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
