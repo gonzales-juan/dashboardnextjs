c20159b52610e08b007a5e4e2412074c85eedd29
Juan-Carlos <juan-carlos.gonzales@technisem.com>
2024-02-29 15:21:28 +0100

    update latest invoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,14 +2,14 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {fetchLatestInvoices,fetchCardData } from '@/app/lib/data';
+import {fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton,LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
 export const dynamic = "force-dynamic"
 export default async function Page() {
   
-  const latestInvoices = await fetchLatestInvoices()
+ 
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -35,8 +35,12 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
+
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+          <LatestInvoices />
+        </Suspense>
        
-        {<LatestInvoices latestInvoices={latestInvoices} />}
+        
       </div>
     </main>
   );
