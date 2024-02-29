11dff50c13aee803ff6e06913d79448386c2a179
Juan-Carlos <juan-carlos.gonzales@technisem.com>
2024-02-29 11:03:00 +0100

    update

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,7 +10,7 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
-export const dynamic = "force-dynamic"
+
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
